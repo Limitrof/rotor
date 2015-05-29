9f96348a1f8c35ab41dd23c07af469cfa87487e6
alex_win_home <zdan@bk.ru>
2015-05-29 16:59:44 +0300

    20150529_rotor

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-Type" content="text/html; charset=utf8"/>
+<link href="css.css" type="text/css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+<script src="js.js"></script>
+</head>
+<body>
+<div id="divCenter">	
+	<ul>
+		<li data-position="0" id="id0"></li>
+		<li data-position="1" id="id1"></li>
+		<li data-position="2" id="id2"></li>
+		<li data-position="3" id="id3"></li>
+		<li data-position="4" id="id4"></li>
+		<li data-position="5" id="id5"></li>
+		<li data-position="6" id="id6"></li>
+		<li data-position="7" id="id7"></li>
+		<li data-position="8" id="id8"></li>
+		<li data-position="9" id="id9"></li>
+	</ul>
+</div>
+
+<div id="divRotorTips">
+	<div class="arrLeft"></div>
+	<div class="arrRight"></div>
+</div>
+</body>
+</html>
---
+++ js.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function(){
+var arrOfImgRotor=[ ["towns/Agra.png","Агра"],["towns/Athens.png","Афины"],["towns/Berlin.png","Берлин"],["towns/Istanbul.png","Стамбул"],["towns/London.png","Лондон"],["towns/Paris.png","Париж"],["towns/Prague.png","Прага"],["towns/Rome.png","Рим"],["towns/Stockholm.png","Стокгольм"],["towns/Vienna.png","Вена"]];
+ ///////////////////
+ // rotor drowing //
+for(var i=0;i<arrOfImgRotor.length;i++){
+	//добавляем переключатели для всех элементов
+	if(i==2) {
+		//для центрального элемента переключатель
+		$('#divRotorTips').append("<span class='on' rel='"+i+"'></span>");
+	} else {
+		$('#divRotorTips').append("<span rel='"+i+"'></span>");
+	}
+	//наполняем пять li-элементов
+	//if(i<=4) {
+		if(i==2){
+			//для центрального элемента увеличиваем высоту
+			$("#id"+i).addClass("bigLi").html("<img src='"+arrOfImgRotor[i][0]+"'/><span>"+arrOfImgRotor[i][1]+"</span>");
+			
+		} else {
+			$("#id"+i).html("<img src='"+arrOfImgRotor[i][0]+"'/><span>"+arrOfImgRotor[i][1]+"</span>");
+		}
+	//}
+	//
+ }
+ // END rotor drowing //
+ ///////////////////////
+var normalPos=true;
+$(".arrRight").click(function(){
+	var currPos=$(".bigLi").attr("data-position");
+	$(".bigLi").removeClass("bigLi");
+	//бегунок
+	var currOn=$(".on");
+	currOn.removeClass("on");
+	if (++currPos>9) {//если вышли за нижнюю границу массива - переходим к посленему элементу
+		currPos=0;
+		$("span[rel='0']").addClass("on");
+	} else {
+		currOn.next().addClass("on");
+	}
+	
+	if (currPos==8 && normalPos==true) {
+		var forForvard=$('li').slice( 0, 5 ).detach();
+		var somer=parseInt($("#divCenter ul").css("margin-left"))+1000;//to do set NORMAL every LI size 200px;
+		$("#divCenter ul").css("margin-left",somer+"px");
+		$("ul").append(forForvard);
+		normalPos=false;
+	} else if (currPos==2 && normalPos!=true) {
+		var forForvard=$('li').slice( 0, 5 ).detach();
+		var somer=parseInt($("#divCenter ul").css("margin-left"))+1000;
+		$("#divCenter ul").css("margin-left",somer+"px");
+		$("ul").append(forForvard);
+		normalPos=true;
+	}
+	$("#divCenter ul").animate({"margin-left":"-=200px"},"slow");
+	$("#id"+currPos).addClass("bigLi",2000);
+});
+$(".arrLeft").click(function(){
+	var currPos=$(".bigLi").attr("data-position");
+	$(".bigLi").removeClass("bigLi");
+	//бегунок
+	var currOn=$(".on");
+	currOn.removeClass("on");
+	//если вышли за нижнюю границу массива - переходим к посленему элементу
+	if(--currPos<0) {
+		currPos=9;
+		$("span[rel='9']").addClass("on");
+	} else {
+		currOn.prev().addClass("on");
+	}
+	
+	if (currPos==7 && normalPos!=true) {
+		var forForvard=$('li').slice( 5, 10 ).detach();
+		var somer=parseInt($("#divCenter ul").css("margin-left"))-1000;//to do set NORMAL every LI size 200px;
+		$("#divCenter ul").css("margin-left",somer+"px");
+		$("ul").prepend(forForvard);
+		normalPos=true;
+	} else if (currPos==1 && normalPos==true) {
+		var forForvard=$('li').slice( 5, 10 ).detach();
+		var somer=parseInt($("#divCenter ul").css("margin-left"))-1000;
+		$("#divCenter ul").css("margin-left",somer+"px");
+		$("ul").prepend(forForvard);
+		normalPos=false;
+	}
+	
+	$("#divCenter ul").animate({"margin-left":"+=200px"},"slow");
+	$("#id"+currPos).addClass("bigLi",2000);
+});
+
+});
